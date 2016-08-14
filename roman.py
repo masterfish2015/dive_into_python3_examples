b2c2670772c65d8841f7d1397f75908ffa6527eb
ldc <925341256@qq.com>
2016-08-14 15:05:21 +0800

    增加对罗马数字转换的输入检查

--- roman.py
+++ roman.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import re
 
 roman_numeral_map = (
     ('M',1000),
@@ -16,8 +17,19 @@ roman_numeral_map = (
     ('IV', 4),
     ('I',1))
 
+roman_numeral_pattern = re.compile(
+     '''^                   # beginning of string
+        M{0,3}              # thousands: 0 to 3 Ms
+        (CM|CD|D?C{0,3})    # hundreds : 900(CM),400(CD), 0-300(0 to 3Cs), or 500-800(D, followed by 0 to 3 Cs)
+        (XC|XL|L?X{0,3})    # tens     : 90(XC), 40(XL), 0-30(0 to 3 Xs) or 50-80(L, followed by 0 to 3 Xs)
+        (IX|IV|V?I{0,3})    # ones     : 9(IX), 4(IV), 0-3(0 to 3 Is) or 5-8(V, followed by 0 to 3 Is)
+        $                   # end of string
+     ''', re.VERBOSE
+)
+
 class OutOfRangeError(ValueError): pass
 class NotIntegerError(ValueError): pass
+class InvalidRomanNumeralError(ValueError):pass
 
 def to_roman(n):
     '''convert integer to Roman numeral '''
@@ -33,4 +45,17 @@ def to_roman(n):
             result += numeral
             n -= integer
             #print('{0} from input, adding {1} to output ({2}).'.format(integer, numeral, n))
+    return result
+
+def from_roman(s):
+    '''convert Roman numeral to integer '''
+    if not roman_numeral_pattern.search(s):
+        raise InvalidRomanNumeralError('Invalid Roman numeral: {0}'.format(s))
+
+    result = 0
+    index = 0
+    for numeral, integer in roman_numeral_map:
+        while s[index:index+len(numeral)]==numeral:
+            result += integer
+            index  += len(numeral)
     return result
--- roman_test.py
+++ roman_test.py
@@ -39,6 +39,12 @@ class KnownValues(unittest.TestCase):
             result = roman.to_roman(integer)
             self.assertEqual(numeral, result)
 
+    def test_from_roman_known_values(self):
+        '''test from_roman functon'''
+        for integer, numeral in self.known_values:
+            result = roman.from_roman(numeral)
+            self.assertEqual(integer, result)
+
 class ToRomanBadInput(unittest.TestCase):
     def test_to_roman_too_large(self):
         '''to_roman should fail with large input '''
@@ -56,6 +62,28 @@ class ToRomanBadInput(unittest.TestCase):
         '''to_roman should fail with non-integer input'''
         self.assertRaises(roman.NotIntegerError,roman.to_roman, 0.5)
 
+class RoundtripCheck(unittest.TestCase):
+    def test_roundtrip(self):
+        for integer in range(1,4000):
+            numeral = roman.to_roman(integer)
+            result = roman.from_roman(numeral)
+            self.assertEqual(integer, result)
+
+class FromRomanBadInput(unittest.TestCase):
+    def test_too_many_repeated_numerals(self):
+        '''from_roman should fail with too many repeated numerals'''
+        for s in ('MMMM','DD','CCCC','LL','XXXX','VV', 'IIII'):
+            self.assertRaises(roman.InvalidRomanNumeralError, roman.from_roman, s)
+
+    def test_repeated_pairs(self):
+        '''from_roman should fail with repeated pairs of numerals'''
+        for s in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
+            self.assertRaises(roman.InvalidRomanNumeralError, roman.from_roman, s)
+
+    def test_malformed_antecedents(self):
+        '''from_roman should fail with malformed antecedents '''
+        for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV', 'MCMC', 'XCX', 'IVI', 'LM', 'LD'):
+            self.assertRaises(roman.InvalidRomanNumeralError, roman.from_roman, s)
 
 if __name__ == '__main__':
     unittest.main()
